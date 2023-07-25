4a1f84ccf7f8d710749411db2d3b9e30e8b36bcf
AbdAlRahmanGad <abdo.gta22@gmail.com>
2023-07-26 02:07:18 +0300

    some progress

--- main.c
+++ main.c
@@ -7,8 +7,55 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-void printAll(FILE *pFile);
+//void printAll(FILE *pFile);
+void errMessage(){
+    char error_message[30] = "An error has occurred\n";
+    write(STDERR_FILENO, error_message, strlen(error_message));
+}
+void lsCommand( char *cmd_argv[10],char *args[]){
+        char *binaryPath = "/bin/ls";
 
+    cmd_argv[0] = binaryPath;
+    int i = 1;
+    for (; i < 5; ++i) {
+        if(args[i]== NULL){
+            cmd_argv[i] = NULL;
+            break;
+        }
+        cmd_argv[i] = args[i];
+    }
+    char* path =  strdup(cmd_argv[i-1]);
+    while ((cmd_argv[i-1] = strdup(strsep(&path, "\n"))) != NULL ) {
+        break;
+    }
+cmd_argv[0]="ls";
+    execv(binaryPath, cmd_argv);
+}
+void catCommand( char *cmd_argv[10],char *args[]){
+    char *binaryPath = "/bin/cat";
+    cmd_argv[0] = binaryPath;
+    int i = 1;
+    for (; i < 5; ++i) {
+        if(args[i]== NULL){
+            cmd_argv[i] = NULL;
+            break;
+        }
+        cmd_argv[i] = args[i];
+    }
+    char* path =  strdup(cmd_argv[i-1]);
+    while ((cmd_argv[i-1] = strdup(strsep(&path, "\n"))) != NULL ) {
+        break;
+    }
+    cmd_argv[i]= NULL;
+    execv(binaryPath, cmd_argv);
+
+}
+void pwdCommand( char *cmd_argv[10]){
+    char *binaryPath = "/bin/pwd";
+    cmd_argv[0] = binaryPath;
+    cmd_argv[1] = NULL;
+    execv(binaryPath, cmd_argv);
+}
 int main(int argc , char *argv[]) {
     printf("wish> ");
     char *text = NULL;
@@ -21,16 +68,16 @@ int main(int argc , char *argv[]) {
     ///// exit done
     ////// cat done
 
-    ////// modify cat 
 
     ////// pwd
     ////// look at the commnad list
     ////// look at the commmands -l  -wall    {if statement}
     /////
-    ////[		cp		dd		expr		launchctl	mkdir		pwd		sleep		test
-    //bash		csh		df		hostname	link		mv		rm		stty		unlink
-    //cat		dash		echo		kill		ln		pax		rmdir		sync		wait4path
-    //chmod		date		ed		ksh		ls		ps		sh		tcsh		zsh
+    ////[
+    /// 	cp		mkdir		pwd
+    //mv		rm
+    //cat			rmdir
+    //cls
 
 
 
@@ -46,6 +93,7 @@ int main(int argc , char *argv[]) {
             arguments[cnt]=s;
             cnt++;
         }
+        arguments[cnt] = NULL;
         int x=0;
             while ((s = strsep(&tmp, " ")) != NULL ) {
                 if(x==0) {
@@ -55,50 +103,49 @@ int main(int argc , char *argv[]) {
                         else
                             printf("%s","ERROR");
 //                            errorMessage();
-                    } else if ((strcmp(s, "ls") == 0 || strcmp(s, "ls\n") == 0)&& cnt == 1) {
+                    } else if ((strcmp(s, "ls") == 0 || strcmp(s, "ls\n") == 0)) {
                         commandNow = 1;
                         rc = fork();
                     } else if (strcmp(s, "cd") == 0) {
                         commandNow = 2;
                         if(cnt == 2)
                         rc = fork();
-                        else
-                            printf("%s","ERROR");
+//                        else
+//                            printf("%s","ERROR");
 //                            errorMessage();
                     }else if (strcmp(s, "cat") == 0){
                         commandNow = 3;
-                        if(cnt == 2)
                             rc = fork();
-                        else
-                            printf("%s","ERROR");
+//                        else
+//                            printf("%s","ERROR");
 //                            errorMessage();
+                    }else if ((strcmp(s, "pwd") ==0||strcmp(s, "pwd\n")== 0) && cnt == 1){
+                        commandNow = 4;
+                        rc = fork();
+
                     }
                     if (rc == 0) {
-//                printf("child pid: %d %d\n", (int) getpid(), x);
                         char *cmd_argv[10];
                         if (commandNow == 1) {
-                            cmd_argv[0] = strdup("/bin/ls");
-                            cmd_argv[1] = NULL;
-                            execv(cmd_argv[0], cmd_argv);
+                            lsCommand(cmd_argv,arguments);
                         } else if (commandNow == 2) {
                             char* path;
-
                             while ((path = strsep(&arguments[1], "\n")) != NULL ) {
                                 break;
                             }
-
                                 if (chdir(path) == -1) {
-                                printf("%s", arguments[1]);
-                                printf("%s\n", "error");
-                                return 0;
+                                    errMessage();
                             }
                         }else if (commandNow == 3){
-                            char* path;
-                            while ((path = strsep(&arguments[1], "\n")) != NULL ) {
-                                break;
-                            }
-                            FILE* fp = fopen(path, "r");
-                            printAll(fp);
+//                            char* path;
+//                            while ((path = strsep(&arguments[1], "\n")) != NULL ) {
+//                                break;
+//                            }
+                            catCommand(cmd_argv,arguments);
+//                            FILE* fp = fopen(path, "r");
+//                            printAll(fp);
+                        }else if (commandNow == 4){
+                            pwdCommand(cmd_argv);
                         }
 // do some set up work
 //                (void) close(STDOUT_FILENO); // no longer can print to the screen
@@ -106,7 +153,7 @@ int main(int argc , char *argv[]) {
 // if successful, doesn't return
 //                        printf("failed!\n");
                     }
-
+/// repository
                 }x++;
         }
         if(strcmp(text,"exit\n") == 0){
@@ -119,13 +166,14 @@ int main(int argc , char *argv[]) {
     return 0;
 }
 
-void printAll(FILE *fp) {
-    char * line = NULL;
-    size_t len = 0;
-    if (fp == NULL){
-        exit(EXIT_FAILURE);
-    }
-    while ((getline(&line, &len, fp)) != -1) {
-        printf("%s", line);
-    }
-}
+
+//void printAll(FILE *fp) {
+//    char * line = NULL;
+//    size_t len = 0;
+//    if (fp == NULL){
+//        exit(EXIT_FAILURE);
+//    }
+//    while ((getline(&line, &len, fp)) != -1) {
+//        printf("%s", line);
+//    }
+//}
